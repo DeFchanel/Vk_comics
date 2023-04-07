420359cfa2a57d069b35f21891eebaf4e1cb8e83
DeFchanel <georgyasr@mail.ru>
2023-04-07 19:50:41 +0300

    Fixed improvements

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests
 import os
 from dotenv import load_dotenv
 import random
+import logging
+
+
+def check_vk_error(response):
+    if response.get('error'):
+        raise requests.exceptions.HTTPError
 
 
 def download_img(file_path, url):
@@ -12,11 +18,8 @@ def download_img(file_path, url):
 
 
 def download_comic():
-    filename = 'comics.png'
-    dir_name = 'images'
+    file_path = 'comics.png'
     url = 'https://xkcd.com/info.0.json'
-    os.makedirs(dir_name, exist_ok=True)
-    file_path = os.path.join(dir_name, filename)
     response = requests.get(url)
     response.raise_for_status()
     number_of_comics = response.json()['num']
@@ -29,8 +32,6 @@ def download_comic():
     return comic_response.json()['alt']
     
 
-
-
 def get_upload_url(access_token, group_id):
     payload = {
         'group_id': group_id,
@@ -39,36 +40,39 @@ def get_upload_url(access_token, group_id):
     }
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     response = requests.get(url, params=payload)
+    check_vk_error(response.json())
     response.raise_for_status()
     return response.json()['response']['upload_url']
 
 
 def upload_photo(upload_url):
-    with open('images/comics.png', 'rb') as file:
+    with open('comics.png', 'rb') as file:
         files = {
             'photo': file,
         }
         response = requests.post(upload_url, files=files)
-        response.raise_for_status()
-        return response.json()
+    check_vk_error(response.json())
+    response.raise_for_status()
+    return response.json()
 
 
-def upload_photo_albom(server, photo, hash):
+def save_wall_photo(photo_server, photo, photo_hash, group_id, access_token):
         payload = {
-            'server': server,
+            'server': photo_server,
             'photo': photo,
-            'hash': hash,
+            'hash': photo_hash,
             'group_id': group_id,
             'access_token': access_token,
             'v': 5.131
         }
         url = 'https://api.vk.com/method/photos.saveWallPhoto'
         response = requests.post(url, params=payload)
+        check_vk_error(response.json())
         response.raise_for_status()
         return response.json()
 
 
-def publick_photo(from_group, attachments, message, access_token):
+def post_photo(from_group, attachments, message, access_token, group_id):
     payload = {
         'owner_id': f'-{group_id}',
         'from_group': from_group,
@@ -79,24 +83,29 @@ def publick_photo(from_group, attachments, message, access_token):
         }
     url = 'https://api.vk.com/method/wall.post'
     response = requests.post(url, params=payload)
+    check_vk_error(response.json())
     response.raise_for_status()
     return response.json()
 
 
 if __name__ == "__main__":
-    message = download_comic()
     load_dotenv()
-    access_token = os.getenv('ACCESS_TOKEN')
-    group_id = os.getenv('GROUP_ID')
-    upload_url = get_upload_url(access_token, group_id)
-    response = upload_photo(upload_url)
-    server = response['server']
-    photo = response['photo']
-    hash = response['hash']
-    albom_response = upload_photo_albom(server, photo, hash)
-    owner_id = albom_response['response'][0]['owner_id']
-    media_id = albom_response['response'][0]['id']
-    attachments = f'photo{owner_id}_{media_id}'
-    from_group = 1
-    publick_photo(from_group, attachments, message, access_token)
-    os.remove('images/comics.png')
+    vk_access_token = os.environ['ACCESS_TOKEN']
+    vk_group_id = os.environ['GROUP_ID']
+    try:
+        message = download_comic()
+        upload_url = get_upload_url(vk_access_token, vk_group_id)
+        response = upload_photo(upload_url)
+        photo_server = response['server']
+        photo = response['photo']
+        photo_hash = response['hash']
+        album_response = save_wall_photo(photo_server, photo, photo_hash, vk_group_id, vk_access_token)
+        owner_id = album_response['response'][0]['owner_id']
+        media_id = album_response['response'][0]['id']
+        attachments = f'photo{owner_id}_{media_id}'
+        from_group = 1
+        post_photo(from_group, attachments, message, vk_access_token, vk_group_id)
+    except requests.exceptions.HTTPError:
+        logging.exception('Произошла ошибка при запросе к ВК или к xkcd')
+    finally:
+        os.remove('comics.png')
